ff65ff1c09832c440f97ccf7e8ad521468b87b28
unknown <fariasmauricio11@outlook.com>
2025-11-11 09:09:58 -0300

    Update files

--- src/components/slide/inputZone/InputZone.tsx
+++ src/components/slide/inputZone/InputZone.tsx
@@ -1,37 +1,196 @@
+import { useState, type ChangeEvent } from "react";
 import { useTabsContext } from "../../../contexts/tabsContext";
 import { TabsNavegate } from "../../TabsNavegate";
 
-function InputZone() {
-  const { tabIndex } = useTabsContext();
+function TabContentInput({
+  tabIndexInput,
+  scriptInput,
+  handleOnChangeScript,
+}: {
+  tabIndexInput: number;
+  scriptInput: string;
+  handleOnChangeScript: (e: ChangeEvent<HTMLTextAreaElement>) => void;
+}) {
+  const htmlInput = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
 
-  function TabContent() {
-    switch (tabIndex) {
-      case 0:
-        return (
-          <textarea className="resize-none font-mono text-white outline-none  p-4 min-w-full min-h-[230px] rounded-tl-none! rounded-[10px] bg-[#424141]" />
-        );
-      case 1:
-        return (
-          <textarea className="resize-none font-mono text-white outline-none p-4 min-h-[230px] min-w-full  rounded-tl-none! rounded-[10px] bg-[#424141]" />
-        );
-      case 2:
-        return (
-          <textarea className="resize-none font-mono text-white outline-none p-4 min-w-full min-h-[230px] overflow-auto rounded-tl-none! rounded-[10px] bg-[#424141]" />
-        );
-      default:
-        break;
-    }
+    <style>
+      .container {
+        padding: 1rem;
+        border: 1px solid black;
+        width: 500px;
+        height: 300px;
+        overflow: hidden;
+      }
+
+      .galeria {
+        margin-inline: auto;
+        display: flex;
+        flex-wrap: wrap;
+        gap: 1rem;
+      }
+
+      img,
+      .galeria-img {
+        display: block;
+        max-width: 100%;
+        height: 100%;
+        width: 130px;
+        border-radius: 0.5rem;
+        aspect-ratio: 1;
+      }
+
+      .imgContainer {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 1rem;
+      }
+
+      /* .capas {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 1rem;
+      }
+
+      .capas > img {
+        display: block;
+        max-width: 100%;
+        height: 100%;
+        width: 90px;
+        border-radius: 0.5rem;
+        aspect-ratio: 1;
+      } */
+    </style>
+  </head>
+  <body>
+    <main class="container">
+      <div class="galeria"></div>
+    </main>
+  </body>
+
+  <script type="module" src="script.js"></script>
+</html>
+  `;
+
+  const dataInput = `
+    export const albuns = [
+  {
+    imagem: "./img/agepe.webp",
+    artista: "Agepê",
+    titulo: "Moro Onde Não Mora Ninguém",
+    ano: 1975,
+    pais: "Brasil",
+    estilo: ["Samba"],
+  },
+  {
+    imagem: "./img/alceu_valenca.webp",
+    artista: "Alceu Valença",
+    titulo: "Cavalo De Pau",
+    ano: 1982,
+    pais: "Brasil",
+    estilo: ["MPB", "Forró", "Reggae"],
+  },
+  {
+    imagem: "./img/catia.webp",
+    artista: "Cátia de França",
+    titulo: "20 Palavras Ao Redor Do Sol",
+    ano: 1979,
+    pais: "Brasil",
+    estilo: ["MPB", "Forró", "Folk Rock"],
+  },
+  {
+    imagem: "./img/ednardo.webp",
+    artista: "Ednardo",
+    titulo: "O Romance Do Pavão Mysteriozo",
+    ano: 1974,
+    pais: "Brasil",
+    estilo: ["MPB", "Folk Rock"],
+  },
+
+  {
+    imagem: "./img/gilberto_gil.webp",
+    artista: "Gilberto Gil",
+    titulo: "Refazenda",
+    ano: 1975,
+    pais: "Brasil",
+    estilo: ["Samba", "MPB"],
+  },
+];
+  `;
+
+  switch (tabIndexInput) {
+    case 0:
+      return (
+        <textarea
+          value={scriptInput}
+          onChange={handleOnChangeScript}
+          className="text-[12px] resize-none font-mono text-white outline-none p-4 min-w-full min-h-[230px] rounded-tl-none! rounded-[10px] bg-[#424141]"
+        />
+      );
+    case 1:
+      return (
+        <textarea
+          value={htmlInput}
+          disabled={true}
+          className="text-[12px] resize-none select-none font-mono text-white outline-none p-4 min-h-[230px] min-w-full  rounded-tl-none! rounded-[10px] bg-[#424141]"
+        />
+      );
+    case 2:
+      return (
+        <textarea
+          value={dataInput}
+          disabled={true}
+          className="text-[12px] resize-none font-mono text-white outline-none p-4 min-w-full min-h-[230px] overflow-auto rounded-tl-none! rounded-[10px] bg-[#424141]"
+        />
+      );
+    default:
+      return null;
   }
+}
+
+function InputZone() {
+  const { tabIndexInput } = useTabsContext();
+  const [scriptInput, setScriptInput] = useState<string>("");
+
+  const handleOnChangeScript = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    const value = event.target.value;
+
+    setScriptInput(value);
+  };
+
+  const handleOnClickRun = () => {
+    console.log(scriptInput);
+  };
 
   return (
     <div>
       <TabsNavegate zoneType="inputZone" />
-      <TabContent />
+      <TabContentInput
+        tabIndexInput={tabIndexInput}
+        scriptInput={scriptInput}
+        handleOnChangeScript={handleOnChangeScript}
+      />
 
-      <div>
-        <button>RESETAR</button>
-        <button>RODA</button>
-        <button>Próximo</button>
+      <div className="flex  items-center justify-between">
+        <button className="p-2 cursor-pointer bg-[#000000] text-[#4780D5] rounded-[10px]">
+          RESETAR
+        </button>
+        <div>
+          <button
+            onClick={handleOnClickRun}
+            className="p-2 cursor-pointer bg-[#D54747] text-[#FFFFFF] rounded-[10px]"
+          >
+            RODAR
+          </button>
+          <button className="ml-5 opacity-50 p-2 cursor-pointer bg-[#000000] text-[#FFFFFF] rounded-[10px]">
+            Próximo
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/components/slide/outputZone/OutputZone.tsx
+++ src/components/slide/outputZone/OutputZone.tsx
@@ -1,50 +1,94 @@
 import { useTabsContext } from "../../../contexts/tabsContext";
 import { TabsNavegate } from "../../TabsNavegate";
 
-function OutputZone() {
+function TabContent() {
   const { tabIndex } = useTabsContext();
 
-  function TabContent() {
-    switch (tabIndex) {
-      case 0:
-        return (
-          <div className="bg-white border-8 border-[#D9D9D9] min-w-[600px] min-h-[250px] rounded-tl-none! rounded-[10px]"></div>
-        );
-      case 1:
-        return (
-          <div className="text-white font-mono p-4 min-w-[600px] border-[#D9D9D9] border-8 min-h-[250px] rounded-tl-none! rounded-[10px] bg-[#6F777C]">
-            // console
-          </div>
-        );
-      case 2:
-        return (
-          <div className="p-4 text-white min-w-[600px] max-h-[250px] overflow-auto rounded-tl-none! rounded-[10px] bg-[#6F777C]">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
-              suscipit magnam nemo eaque modi illum ipsa iste, ipsam, non cumque
-              exercitationem sequi laudantium aperiam nobis incidunt consectetur
-              explicabo reprehenderit ad.
-            </p>
-            <p className="mt-4">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
-              suscipit magnam nemo eaque modi illum ipsa iste, ipsam, non cumque
-              exercitationem sequi laudantium aperiam nobis incidunt consectetur
-              explicabo reprehenderit ad.
-            </p>
-            <p className="mt-4">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
-              suscipit magnam nemo eaque modi illum ipsa iste, ipsam, non cumque
-              exercitationem sequi laudantium aperiam nobis incidunt consectetur
-              explicabo reprehenderit ad.
-            </p>
-          </div>
-        );
+  const htmlInput = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <style>
+      .container {
+        padding: 1rem;
+        border: 1px solid black;
+        width: 500px;
+        height: 300px;
+        overflow: hidden;
+      }
+      .galeria {
+        margin-inline: auto;
+        display: flex;
+        flex-wrap: wrap;
+        gap: 1rem;
+      }
+      img, .galeria-img {
+        display: block;
+        max-width: 100%;
+        height: 100%;
+        width: 130px;
+        border-radius: 0.5rem;
+        aspect-ratio: 1;
+      }
+      .imgContainer {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 1rem;
+      }
+    </style>
+  </head>
+  <body>
+    <main class="container">
+      <div class="galeria"></div>
+    </main>
+  </body>
+</html>
+  `;
 
-      default:
-        break;
-    }
+  switch (tabIndex) {
+    case 0:
+      return (
+        <div
+          className="bg-white border-8 border-[#D9D9D9] min-w-[600px] max-h-[250px] overflow-auto rounded-tl-none! rounded-[10px]"
+          dangerouslySetInnerHTML={{ __html: htmlInput }}
+        />
+      );
+    case 1:
+      return (
+        <div className="text-white font-mono p-4 min-w-[600px] border-[#D9D9D9] border-8 min-h-[250px] rounded-tl-none! rounded-[10px] bg-[#6F777C]">
+          // console
+        </div>
+      );
+    case 2:
+      return (
+        <div className="p-4 text-white min-w-[600px] max-h-[250px] overflow-auto rounded-tl-none! rounded-[10px] bg-[#6F777C]">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+            suscipit magnam nemo eaque modi illum ipsa iste, ipsam, non cumque
+            exercitationem sequi laudantium aperiam nobis incidunt consectetur
+            explicabo reprehenderit ad.
+          </p>
+          <p className="mt-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+            suscipit magnam nemo eaque modi illum ipsa iste, ipsam, non cumque
+            exercitationem sequi laudantium aperiam nobis incidunt consectetur
+            explicabo reprehenderit ad.
+          </p>
+          <p className="mt-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+            suscipit magnam nemo eaque modi illum ipsa iste, ipsam, non cumque
+            exercitationem sequi laudantium aperiam nobis incidunt consectetur
+            explicabo reprehenderit ad.
+          </p>
+        </div>
+      );
+    default:
+      return null;
   }
-
+}
+function OutputZone() {
   return (
     <div>
       <TabsNavegate zoneType="outputZone" />
